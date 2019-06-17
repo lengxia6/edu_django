a47dc3a392da102e07b762b8502e25c01393d43e
lengxia <13071142513@163.com>
2019-06-17 14:53:09 +0800

    第六节:9.1.模板和urls功能、9.2.章节和视频功能、9.3.资料下载功能、9.4.讲师提示功能、9.5.课程评论功能

--- MxOnline/apps/course/models.py
+++ MxOnline/apps/course/models.py
@@ -2,7 +2,7 @@ from datetime import  datetime
 
 
 from django.db import models
-from organization.models import CourseOrg
+from organization.models import CourseOrg,Teacher
 
 # Create your models here.
 
@@ -36,6 +36,10 @@ class Course(models.Model):
     add_time = models.DateTimeField("添加时间",default=datetime.now)
     course_org = models.ForeignKey(CourseOrg, on_delete=models.CASCADE, verbose_name="所属机构", null=True, blank=True)
     category = models.CharField("课程类别",max_length=20,default="")
+    teacher = models.ForeignKey(Teacher,verbose_name='讲师',null=True,blank=True,on_delete=models.CASCADE)
+    youneed_know = models.CharField('课程须知',max_length=300,default='')
+    teacher_tell = models.CharField('老师告诉你', max_length=300, default='')
+
 
     class Meta:
         verbose_name = "课程"
@@ -45,6 +49,10 @@ class Course(models.Model):
     def get_zj_nums(self):
         # 获取课程的章节数
         return self.lesson_set.all().count()
+
+    def get_course_lesson(self):
+        # 获取课程的章节
+        return self.lesson_set.all()
 
     def get_learn_users(self):
         # 获取这门课程的学习用户
@@ -63,6 +71,10 @@ class Lesson(models.Model):
         verbose_name = "章节"
         verbose_name_plural = verbose_name
 
+    def get_lesson_vedio(self):
+        # 获取章节所有视频
+        return self.video_set.all()
+
     def __str__(self):
         return '《{0}》课程的章节 >> {1}'.format(self.course, self.name)
 
@@ -71,6 +83,8 @@ class Lesson(models.Model):
 class Video(models.Model):
     lesson = models.ForeignKey(Lesson, verbose_name="章节",on_delete=models.CASCADE)
     name = models.CharField("视频名",max_length=100)
+    url = models.CharField('访问地址',default='',max_length=200)
+    learn_times = models.IntegerField("学习时长(分钟数)",default=0)
     add_time = models.DateTimeField("添加时间", default=datetime.now)
 
     class Meta:
--- MxOnline/apps/course/views.py
+++ MxOnline/apps/course/views.py
@@ -1,9 +1,10 @@
 
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.views.generic import View
-from .models import Course
+from .models import Course,CourseResource
+from operation.models import UserFavorite,CourseComments
 from pure_pagination import Paginator,EmptyPage,PageNotAnInteger
 
 class CourseListView(View):
@@ -37,6 +38,7 @@ class CourseListView(View):
             "sort":sort,
             "hot_courses":hot_courses,
 
+
         })
 
 
@@ -50,22 +52,109 @@ class CourseDetailView(View):
         course.save()
         # 课程标签
         # 通过当前标签，查找数据库中的课程
+        has_fav_course = False
+        has_fav_org = False
+
+        # 必须是用户已登录我们才需要判断
+        if request.user.is_authenticated:
+            if UserFavorite.objects.filter(user=request.user,fav_id=course.id,fav_type=1):
+                has_fav_course = True
+
+            if UserFavorite.objects.filter(user=request.user,fav_id=course.course_org.id,fav_type=2):
+                has_fav_org = True
+
         tag = course.tag
         if tag:
             # 需要从1开始不然会推荐自己
-            relate_courses = Course.objects.filter(tag=tag)[:3]
+            relate_courses = Course.objects.filter(tag=tag)[:2]
 
         else:
             relate_courses = []
 
-        print(relate_courses)
-
-
 
         return render(request,"course-detail.html",{
             'course':course,
-            'relate_courses':relate_courses
+            'relate_courses':relate_courses,
+            "has_fav_course":has_fav_course,
+            "has_fav_org":has_fav_org,
         })
+
+
+class CourseInfoView(View):
+    '''课程章节信息'''
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        all_resources = CourseResource.objects.filter(course=course)
+        return render(request, "course-video.html", {
+            "course": course,
+            "all_resources":all_resources,
+        })
+
+
+class CommentsView(View):
+
+    """课程评论"""
+
+    def get(self,request,course_id):
+        course = Course.objects.get(id=int(course_id))
+
+        all_resources = CourseResource.objects.filter(course=course)
+        all_comments = CourseComments.objects.all()
+        return render(request,"course-comment.html",{
+            "course":course,
+            "all_resources":all_resources,
+            "al_comments":all_comments,
+        })
+
+
+# 添加评论
+class AddCommentsView(View):
+    """用户评论"""
+
+    def post(self,request):
+        if not request.user.is_authenticated:
+            # 未登录时返回json提示未登录，跳转到登录页面是在ajax中做的
+            return HttpResponse('{"status":"fail","msg":"用户未登录"}',content_type='application/json')
+        course_id = request.POST.get("course_id",0)
+        comments = request.POST.get("comments","")
+
+        if int(course_id) > 0 and comments:
+            # 实例化一个course_comments对象
+
+            course_comments = CourseComments()
+            # 获取评论的是哪门课程
+
+            course = Course.objects.get(id = int(course_id))
+            # 分别把评论的课程、评论的内容和评论的用户保存到数据库
+
+            course_comments.course = course
+
+            course_comments.comments = comments
+
+            course_comments.user = request.user
+
+            return HttpResponse('{"status":"success","msg":"评论成功"}',content_type='application/json')
+
+        else:
+            return HttpResponse('{"status":"fail","msg":"评论失败"}',content_type='application/json')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
