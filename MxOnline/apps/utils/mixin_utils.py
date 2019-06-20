fcf93ab1eebb4e46ce32a2a2fbc6c0b4840956cd
lengxia <13071142513@163.com>
2019-06-21 00:38:34 +0800

    第六节:9.6.相关课程推荐功能、9.7.把课程与用户关联起来功能

---
+++ MxOnline/apps/utils/mixin_utils.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+#Autor:Ziting
+# apps/utils/mixin_utils.py
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+class LoginRequiredMixin(object):
+    @method_decorator(login_required(login_url='/login/'))
+    def dispatch(self,request,*args,**kwargs):
+        return super(LoginRequiredMixin,self).dispatch(request,*args,**kwargs)
+
+
+
+
+
